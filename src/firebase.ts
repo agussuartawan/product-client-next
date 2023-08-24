4f0cc0b6354697f488eb6c05cf1ba99358579bbf
om.hairis <hairismahoree@gmail.com>
2023-08-24 11:20:58 +0800

    init

--- src/firebase.ts
+++ src/firebase.ts
@@ -11,7 +11,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 export function requestPermission() {
     Notification.requestPermission().then((permission) => {
